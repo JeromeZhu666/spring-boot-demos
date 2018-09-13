6a676ef04ea4339af4f9ff0ee0a7c8029587bf2e
JeromeZhu666 <jeromezhu666@gmail.com>
2018-09-13 18:06:49 +0800

    Add spring boot for logback part to project

---
+++ spring-boot-demo-logback/src/main/java/xin/jerome/logback/SpringBootDemoLogbackApplication.java
@@ -0,0 +1,17 @@
+package xin.jerome.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Jerome Zhu
+ * @since 2018.09.13 17:23
+ */
+@SpringBootApplication
+public class SpringBootDemoLogbackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoLogbackApplication.class);
+    }
+}
---
+++ spring-boot-demo-logback/src/main/java/xin/jerome/logback/controller/LogbackController.java
@@ -0,0 +1,29 @@
+package xin.jerome.logback.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 日志控制器
+ *
+ * @author Jerome Zhu
+ * @since 2018.09.13 17:39
+ */
+@RestController
+public class LogbackController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/logback/{msg}")
+    public String getMsg(@PathVariable String msg) {
+        String log = "Logback" + msg;
+
+        logger.info(log);
+
+        return log;
+    }
+
+}
