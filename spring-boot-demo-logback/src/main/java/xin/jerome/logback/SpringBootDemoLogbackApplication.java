99e7b4bac8e60f6fe7f95bfc9df359a230a25735
JeromeZhu666 <jeromezhu666@gmail.com>
2018-12-14 14:27:44 +0800

    完成SpringBoot自定义Logback的实现

--- spring-boot-demo-logback/src/main/java/xin/jerome/logback/SpringBootDemoLogbackApplication.java
+++ spring-boot-demo-logback/src/main/java/xin/jerome/logback/SpringBootDemoLogbackApplication.java
@@ -1,5 +1,7 @@
 package xin.jerome.logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class SpringBootDemoLogbackApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(SpringBootDemoLogbackApplication.class);
+
     public static void main(String[] args) {
+        logger.trace("trace level log.");
+        logger.debug("debug level log.");
+        logger.info("info level log.");
+        logger.warn("warn level log.");
+        logger.error("error level log.");
         SpringApplication.run(SpringBootDemoLogbackApplication.class);
     }
 }
