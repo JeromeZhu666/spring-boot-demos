fb9778e14a153dfaf1fd5fa13623f6348b76f408
JeromeZhu666 <jeromezhu666@gmail.com>
2018-12-17 10:51:35 +0800

    完成SpringBoot使用AOP处理日志的around的实现

--- spring-boot-demo-logback/src/main/java/xin/jerome/logback/aop/WebLogAspect.java
+++ spring-boot-demo-logback/src/main/java/xin/jerome/logback/aop/WebLogAspect.java
@@ -1,10 +1,8 @@
 package xin.jerome.logback.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -29,8 +27,29 @@ public class WebLogAspect {
     @Pointcut("execution(public * xin.jerome.logback.controller..*.*(..))")
     public void webLogPointcut(){}
 
-    @Before("webLogPointcut()")
-    public void doBefore(JoinPoint joinPoint) throws Throwable {
+//    @Before("webLogPointcut()")
+//    public void doBefore(JoinPoint joinPoint) throws Throwable {
+//        // 接收到请求，记录请求内容
+//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+//        HttpServletRequest request = attributes.getRequest();
+//
+//        // 记录下请求内容
+//        logger.info("URL : " + request.getRequestURL().toString());
+//        logger.info("HTTP_METHOD : " + request.getMethod());
+//        logger.info("IP : " + request.getRemoteAddr());
+//        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+//        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+//
+//    }
+//
+//    @AfterReturning(returning = "ret", pointcut = "webLogPointcut()")
+//    public void doAfterReturning(Object ret) throws Throwable {
+//        // 处理完请求，返回内容
+//        logger.info("RESPONSE : " + ret);
+//    }
+
+    @Around("webLogPointcut()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
         // 接收到请求，记录请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
@@ -39,15 +58,14 @@ public class WebLogAspect {
         logger.info("URL : " + request.getRequestURL().toString());
         logger.info("HTTP_METHOD : " + request.getMethod());
         logger.info("IP : " + request.getRemoteAddr());
-        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+        logger.info("CLASS_METHOD : " + pjp.getSignature().getDeclaringTypeName() + "." + pjp.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(pjp.getArgs()));
 
-    }
+        Object ret = pjp.proceed();
 
-    @AfterReturning(returning = "ret", pointcut = "webLogPointcut()")
-    public void doAfterReturning(Object ret) throws Throwable {
         // 处理完请求，返回内容
         logger.info("RESPONSE : " + ret);
+        return ret;
     }
 
 }
